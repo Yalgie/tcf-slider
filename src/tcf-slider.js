04bb5d4dcfd15848faa61a1a8b6b1ed08e4a17e9
Daniel Yalg <daniel@thecodingforge.com.au>
2016-06-20 11:47:18 +1000

    defining elements in js

--- src/tcf-slider.js
+++ src/tcf-slider.js
@@ -1,20 +1,37 @@
+"use strict";
+
 jQuery.fn.tcf_slider = function(options) {
 
     var defaults = {
 
     };
 
-    var settings = $.extend( {}, defaults, options )
+    var settings = $.extend( {}, defaults, options );
 
     var methods = {
         init: function() {
+        	methods.defineElements();
+        	methods.build();
+        },
 
+        defineElements: function() {
+        	settings.eles.wrap = $("<div class='tcf-slider-wrap'>");
+        	settings.eles.imageWrap = $("<div class='tcf-slider-image-wrap'>");
+        	settings.eles.prev = $("<div class='tcf-slider-prev'>");
+        	settings.eles.next = $("<div class='tcf-slider-next'>");
+        	settings.eles.crumbContainer = $("<div class='tcf-slider-crumb-container'>");
+        	settings.eles.crumb = $("<div class='tcf-slider-crumb'>");
+        },
+
+        build: function() {
+        	settings.eles.main.append(settings.eles.wrap);
         }
-    }
+    };
 
     return this.each(function() {
-        settings.elements = {}
-        settings.elements.main = $(this)
-        methods.init(this)
+        settings.eles = {};
+        settings.eles.main = $(this);
+        methods.init(this);
     });
+
 };
