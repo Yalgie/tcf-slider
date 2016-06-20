3a619a96c1ac94a1ac043da47f642986afe08ee2
Daniel Yalg <daniel@thecodingforge.com.au>
2016-06-20 11:58:11 +1000

    updating gulpfiile, laying out build methods for plugin

--- src/tcf-slider.js
+++ src/tcf-slider.js
@@ -11,20 +11,44 @@ jQuery.fn.tcf_slider = function(options) {
     var methods = {
         init: function() {
         	methods.defineElements();
-        	methods.build();
+        	methods.buildWrappers();
+        	methods.buildPrevBtn();
+        	methods.buildImages();
+        	methods.buildNextBtn();
+        	methods.buildCrumbs();
         },
 
         defineElements: function() {
-        	settings.eles.wrap = $("<div class='tcf-slider-wrap'>");
+        	settings.eles.mainWrap = $("<div class='tcf-slider-wrap'>");
         	settings.eles.imageWrap = $("<div class='tcf-slider-image-wrap'>");
-        	settings.eles.prev = $("<div class='tcf-slider-prev'>");
-        	settings.eles.next = $("<div class='tcf-slider-next'>");
-        	settings.eles.crumbContainer = $("<div class='tcf-slider-crumb-container'>");
+        	settings.eles.prevWrap = $("<div class='tcf-slider-prev'>");
+        	settings.eles.nextWrap = $("<div class='tcf-slider-next'>");
+        	settings.eles.crumbWrap = $("<div class='tcf-slider-crumb-container'>");
         	settings.eles.crumb = $("<div class='tcf-slider-crumb'>");
         },
 
-        build: function() {
-        	settings.eles.main.append(settings.eles.wrap);
+        buildWrappers: function() {
+        	settings.eles.main.append(settings.eles.mainWrap);
+        	settings.eles.mainWrap.append(settings.eles.prevWrap);
+        	settings.eles.mainWrap.append(settings.eles.imageWrap);
+        	settings.eles.mainWrap.append(settings.eles.nextWrap);
+        	settings.eles.mainWrap.append(settings.eles.crumbWrap);
+        },
+
+        buildPrevBtn: function() {
+        	// &laquo
+        },
+
+        buildImages: function() {
+
+        },
+
+        buildNextBtn: function() {
+        	// &raquo
+        },
+
+        buildCrumbs: function() {
+
         }
     };
 
